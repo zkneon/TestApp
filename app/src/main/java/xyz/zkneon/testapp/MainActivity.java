54fcbbdc28ae0eb2ab7feec99f46b349058dcdb3
zkh <zk.neon@gmail.com>
2018-01-18 17:41:24 +0300

    Its work

--- app/src/main/java/xyz/zkneon/testapp/MainActivity.java
+++ app/src/main/java/xyz/zkneon/testapp/MainActivity.java
@@ -2,12 +2,25 @@ package xyz.zkneon.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mButton;
+    public TextView nText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String nString = (String) getText(R.string.app_name);
+        nText = findViewById(R.id.textView);
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nText.setText(nString);
+            }
+        });
     }
 }
